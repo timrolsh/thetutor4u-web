6c0d55a64f1519b1a5c1dedd8ceea806d4bbbc84
timrolsh <tim.rolsh@gmail.com>
2022-08-10 23:30:36 -0400

    made google auth better, issue with token expirations

--- server/express_config.js
+++ server/express_config.js
@@ -1,9 +1,8 @@
 const express = require("express");
 const server = express();
-const session = require("express-session");
 const db = require("./db_pool");
-const crypto = require("crypto");
-const passport = require("passport");
+const cookieParser = require("cookie-parser");
+server.use(cookieParser());
 
 // load variables from .env
 require("dotenv").config();
@@ -46,68 +45,4 @@ server.set("view engine", "ejs");
 server.use(express.urlencoded({extended: false}));
 server.use(express.json());
 
-server.use(
-    session({
-        resave: false,
-        saveUninitialized: true,
-        secret: "SECRET"
-    })
-);
-
-server.use(passport.initialize());
-server.use(passport.session());
-
-passport.serializeUser(function (user, cb) {
-    cb(null, user);
-});
-
-passport.deserializeUser(function (obj, cb) {
-    cb(null, obj);
-});
-
-const GoogleStrategy = require("passport-google-oauth2").Strategy;
-
-passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-            callbackURL: "http://localhost/auth/google/passport",
-            scope: ["profile", "email"]
-        },
-        (accessToken, refreshToken, profile, callback) => {
-            db.query("select * from thetutor4u.user where id = $1;", [profile.id])
-                .then((dbResponse) => {
-                    console.log("made query");
-                    // if user does not exist in the database, add a new entry for them
-                    if (dbResponse.rows.length === 0) {
-                        console.log("new user but not found in database, updating database");
-                        db.query(
-                            "insert into thetutor4u.user (id, email, username, first_name, last_name, " +
-                                "time_account_created) values ($1, $2, $3, $4, $5, $6);",
-                            [
-                                profile.id,
-                                profile.emails[0].value,
-                                crypto.randomUUID(),
-                                profile.given_name,
-                                profile.family_name,
-                                Date.now()
-                            ]
-                        ).then(() => {
-                            console.log("inserted new user");
-                            callback(null, profile);
-                        });
-                    } else {
-                        console.log("authenticated user is in database, continuing...")
-                        callback(null, profile);
-                    }
-                })
-                .catch(() => {
-                    console.log("query was unsuccessful");
-                    callback(null, profile);
-                });
-        }
-    )
-);
-
-module.exports = {server: server, passport: passport};
+module.exports = server;
--- server/express_routes.js
+++ server/express_routes.js
@@ -1,59 +1,132 @@
-const {server, passport} = require("./express_config");
+const server = require("./express_config");
 const rootPath = require("./root_path");
 const db = require("./db_pool");
+const Client = require("google-auth-library").OAuth2Client;
+const googleClient = new Client(process.env.CLIENT_ID);
+const crypto = require("crypto");
+const atob = require("atob");
+
+function unixSeconds() {
+    return parseInt(Date.now() / 1000);
+}
+
+// callbacks to a user json
+function getUser(request, response, callback) {
+    if (request.cookies.token) {
+        const user = JSON.parse(atob(request.cookies.token.split(".")[1]));
+        // if token has not expired yet
+        if (validTimeToken(user)) {
+            // if authenticator provider is google
+            if (user.iss === "https://accounts.google.com") {
+                googleClient
+                    .verifyIdToken({
+                        idToken: request.cookies.token,
+                        audience: process.env.GOOGLE_CLIENT_ID
+                    })
+                    .then((response) => {
+                        callback(response.getPayload());
+                    });
+            } else {
+                console.log("identity provider is not google");
+                callback(user);
+            }
+        }
+        // otherwise token is expired,
+        else {
+            response.clearCookie("token");
+            response.redirect("/");
+        }
+    } else {
+        callback(false);
+    }
+}
+
+function validTimeToken(userData) {
+    return userData === true || unixSeconds() < parseInt(userData.exp);
+}
 
 // home page: if user is logged in, bring them to dashboard
 server.get("/", (request, response) => {
-    if (request.isAuthenticated()) {
-        response.redirect("/dashboard");
-    } else {
-        response.sendFile(`${rootPath}/pages/index.html`);
-        response.statusCode = 200;
-    }
+    getUser(request, response, (user) => {
+        if (user === false) {
+            response.sendFile(`${rootPath}/pages/index.html`);
+            response.statusCode = 200;
+        } else {
+            response.redirect("/dashboard");
+        }
+    });
 });
 // dashboard page: if user is not logged in, bring them to home page where they can choose how to log in
 server.get("/dashboard", (request, response) => {
-    if (request.isAuthenticated()) {
-        response.render("dashboard");
-        response.statusCode = 200;
-    } else {
-        response.redirect("/");
-    }
+    getUser(request, response, (user) => {
+        if (user === false) {
+            response.redirect("/");
+        } else {
+            response.render("dashboard");
+            response.statusCode = 200;
+        }
+    });
 });
 
 server.get("/settings", (request, response) => {
-    if (request.isAuthenticated()) {
-        response.render("settings");
-        response.statusCode = 200;
-    } else {
-        response.redirect("/");
-    }
+    getUser(request, response, (user) => {
+        if (user === false) {
+            response.redirect("/");
+        } else {
+            response.render("settings");
+            response.statusCode = 200;
+        }
+    });
 });
 
 server.get("/me", (request, response) => {
-    if (request.isAuthenticated()) {
-        response.send(request.user);
-        response.statusCode = 200;
-    }
+    getUser(request, response, (user) => {
+        if (user === false) {
+            response.redirect("/");
+        } else {
+            response.send(user);
+            response.statusCode = 200;
+        }
+    });
 });
 
 /*
 Authentication routes
 */
 
-// google authentication routes
-server.post("/auth/google/passport", passport.authenticate("google", {failureRedirect: "/"}));
-
-server.get("/auth/google/passport", passport.authenticate("google", {failureRedirect: "/"}), (request, response) => {
-    response.redirect("/dashboard");
+/* google authentication callback route, google posts back here after it is logged in, maybe find a better way of 
+doing it as every time the server is restarted the sessions are lost */
+server.post("/auth/google/callback", (request, response) => {
+    response.clearCookie("token");
+    googleClient
+        .verifyIdToken({
+            idToken: request.body.credential,
+            audience: process.env.GOOGLE_CLIENT_ID
+        })
+        .then((user) => {
+            user = user.getPayload();
+            response.cookie("token", request.body.credential);
+            db.query("select * from thetutor4u.user where id = $1;", [user.sub]).then((dbResponse) => {
+                console.log(`made query for user ${user.name}`);
+                // if user does not exist in the database, add a new entry for them
+                if (dbResponse.rows.length === 0) {
+                    console.log(
+                        `new user ${user.name}, has been authenticated, but they are not found in database. Inserting new user into database.`
+                    );
+                    db.query(
+                        "insert into thetutor4u.user (id, email, username, first_name, last_name, " +
+                            "time_account_created) values ($1, $2, $3, $4, $5, $6);",
+                        [user.sub, user.email, crypto.randomUUID(), user.given_name, user.family_name, Date.now()]
+                    ).then(() => {
+                        console.log(`inserted new user ${user.name}`);
+                        return;
+                    });
+                } else {
+                    console.log(`found user ${user.name}`);
+                }
+                response.redirect("/dashboard");
+            });
+        });
 });
-
-// logout routes for all methods in passport
-server.get("/auth/logout-passport", (request, response) => {
-    request.logout(() => {
-        response.redirect("/");
-    });
-});
-
 
 module.exports = server;
